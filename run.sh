cb62cb5b4856a6e8987511bd55313382ef2afa72
tudor-bodea <tudor_bodea@hotmail.com>
2015-03-08 01:07:17 -0500

    Test Commit

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd C:/Work/Personal/Personal/Coursera/5-ReproducibleResearch/PeerAssessment1
-REM Rscript --vanilla run.R
+Rscript --vanilla run.R
 git status
 git add -A
 git commit -m "Test Commit"
